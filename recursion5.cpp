0109426fbdb526e07ad39bc05f31eafe148114e1
Marco Antonio Paucar Velazco <marco.vela2027@gmail.com>
2026-09-10 16:52:03 -0500

    Create recursion5.cpp

---
+++ recursion5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int encontrarMaximo (int arr[], int n);
+int main () {
+    int n;
+    cout << "Ingrese cantidad de elementos: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cout << "El maximo es: " << encontrarMaximo (arr , n) << endl;
+    return 0;
+}
+
+int encontrarMaximo (int arr[], int n) {
+    if (n == 1) return arr [0];
+    int maxResto = encontrarMaximo (arr , n - 1);
+    return (arr[n - 1] > maxResto) ? arr[n - 1] : maxResto;
+}
