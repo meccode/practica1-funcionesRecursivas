bc8630c1962a3e347ced81f4f7b665328ba688f8
Marco Antonio Paucar Velazco <marco.vela2027@gmail.com>
2026-09-10 16:55:49 -0500

    Update-recursion5.cpp

--- recursion5.cpp
+++ recursion5.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-
-using namespace std;
-
-int encontrarMaximo (int arr[], int n);
-int main () {
-    int n;
-    cout << "Ingrese cantidad de elementos: ";
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    cout << "El maximo es: " << encontrarMaximo (arr , n) << endl;
-    return 0;
-}
-
-int encontrarMaximo (int arr[], int n) {
-    if (n == 1) return arr [0];
-    int maxResto = encontrarMaximo (arr , n - 1);
-    return (arr[n - 1] > maxResto) ? arr[n - 1] : maxResto;
-}
+1 #include <iostream >
+2
+3 using namespace std;
+4
+5 void convertirBinario (int n);
+6
+7 int main () {
+8 int n;
+9 cout << "Ingrese numero decimal: ";
+10 cin >> n;
+11 cout << "Binario: ";
+12 if (n == 0) cout << 0;
+13 else convertirBinario (n);
+14 cout << endl;
+15 return 0;
+16 }
+17
+18 void convertirBinario (int n) {
+19 if (n > 0) {
+20 convertirBinario (n / 2);
+21 cout << (n % 2);
+22 
