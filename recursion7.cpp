c921b7ef7629c099431989ad5d4882d42685c15a
Marco Antonio Paucar Velazco <marco.vela2027@gmail.com>
2026-09-11 13:40:41 -0500

    Update recursion7.cpp

--- recursion7.cpp
+++ recursion7.cpp
@@ -1,18 +1,17 @@
 #include <iostream >
-2
-3 using namespace std;
-4
-5 int imagenEspecular (int n, int rev = 0);
-6
-7 int main () {
-8 int n;
-9 cout << "Ingrese numero positivo: ";
-10 cin >> n;
-11 cout << "Imagen especular: " << imagenEspecular (n) << endl;
-12 return 0;
-13 }
-14
-15 int imagenEspecular (int n, int rev) {
-16 if (n == 0) return rev;
-17 return imagenEspecular (n / 10, rev * 10 + (n % 10));
-18 }
+using namespace std;
+
+ int imagenEspecular (int n, int rev = 0);
+
+int main () {
+ int n;
+ cout << "Ingrese numero positivo: ";
+ cin >> n;
+ cout << "Imagen especular: " << imagenEspecular (n) << endl;
+ return 0;
+ }
+
+ int imagenEspecular (int n, int rev) {
+ if (n == 0) return rev;
+ return imagenEspecular (n / 10, rev * 10 + (n % 10));
+ }
