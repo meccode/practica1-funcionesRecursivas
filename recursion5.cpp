1c5df2ef4c36b479e0a83e634036052dd7912254
Marco Antonio Paucar Velazco <marco.vela2027@gmail.com>
2026-09-11 13:39:58 -0500

    Update recursion5.cpp

--- recursion5.cpp
+++ recursion5.cpp
@@ -1,22 +1,22 @@
-1 #include <iostream >
-2
-3 using namespace std;
-4
-5 void convertirBinario (int n);
-6
-7 int main () {
-8 int n;
-9 cout << "Ingrese numero decimal: ";
-10 cin >> n;
-11 cout << "Binario: ";
-12 if (n == 0) cout << 0;
-13 else convertirBinario (n);
-14 cout << endl;
-15 return 0;
-16 }
-17
-18 void convertirBinario (int n) {
-19 if (n > 0) {
-20 convertirBinario (n / 2);
-21 cout << (n % 2);
-22 
+#include <iostream >
+
+using namespace std;
+
+void convertirBinario (int n);
+
+int main () {
+int n;
+cout << "Ingrese numero decimal: ";
+ cin >> n;
+cout << "Binario: ";
+if (n == 0) cout << 0;
+else convertirBinario (n);
+cout << endl;
+return 0;
+}
+
+void convertirBinario (int n) {
+if (n > 0) {
+convertirBinario (n / 2);
+cout << (n % 2);
+
